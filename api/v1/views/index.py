848a0caf3ed08479dd3b11d8faf828afcd971f5f
issmaile <ismail25smello@gmail.com>
2024-01-10 00:18:39 +0100

    file_storage fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,13 +8,12 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     '''
     Returns a JSON response for RESTful API.
     '''
-    response = {'status': 'OK'}
-    return jsonify(response)
+    return jsonify(status = 'OK')
 
 
 @app_views.route('/stats', methods=['GET'])
